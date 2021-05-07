d819251b91757c2253e194c1ee0bc8043164b547
JANINDU MALEESHA <janindumaleesha99@gmail.com>
2021-05-07 18:49:58 +0530

    delete the validation part and build the intent part

--- app/src/main/java/com/example/mad/emergency1.java
+++ app/src/main/java/com/example/mad/emergency1.java
@@ -1,6 +1,5 @@
 package com.example.mad;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,20 +7,21 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
+
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
-import com.google.firebase.database.ValueEventListener;
+
 
 public class emergency1 extends AppCompatActivity {
 
     Button btnnn1, btnnn2, btnnn3, btnnn4, btnnn5;
+    ImageButton imagebtn2;
     EditText etvv1;
     TextView tvv1, tvv2;
+    DatabaseReference Reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,93 +33,36 @@ public class emergency1 extends AppCompatActivity {
         btnnn3 = findViewById(R.id.btnn3);
         btnnn4 = findViewById(R.id.btnn4);
         btnnn5 = findViewById(R.id.btnn5);
+        imagebtn2 = findViewById(R.id.imageButton20);
         etvv1 = findViewById(R.id.etv1);
         tvv1 = findViewById(R.id.tv1);
         tvv2 = findViewById(R.id.tv2);
 
-
-
-    }
-    private boolean validateUsername(){
-        String val = etvv1.getEditableText().toString();
-
-        if(val.isEmpty()){
-            etvv1.setError("Field cannot be empty");
-            return false;
-        }else if(val.length() >= 10){
-            etvv1.setError("Username too long");
-            return false;
-        }else{
-            etvv1.setError(null);
-            return true;
-        }
-    }
-    public void loginUser(View view){
-        if(!validateUsername() ){
-            return;
-        }
-        else{
-            isUser();
-        }
-    }
-
-    private void isUser() {
-
-        String userEnteredUsername = etvv1.getEditableText().toString().trim();
-
-        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("User");
-
-        Query checkUser = reference.orderByChild("etvv1").equalTo(userEnteredUsername);
-
-        checkUser.addListenerForSingleValueEvent(new ValueEventListener() {
+        imagebtn2.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-
-                if(snapshot.exists()){
-
-                    etvv1.setError(null);
-
-                    String userEnteredUsername = etvv1.getEditableText().toString().trim();
-                    String usernameFromDB = snapshot.child(userEnteredUsername).child("etvv1").getValue(String.class);
-
-                    if(usernameFromDB.equals(userEnteredUsername)){
-
-                        etvv1.setError(null);
-
-                        Integer telFromDB = snapshot.child(userEnteredUsername).child("tel").getValue(Integer.class);
-                        String addressFromDB = snapshot.child(userEnteredUsername).child("address").getValue(String.class);
-
-                        Intent intent = new Intent(getApplicationContext(),emergency1.class);
-
-                        intent.putExtra("tel" ,telFromDB);
-                        intent.putExtra("address", addressFromDB);
-
-                        startActivity(intent);
-                    }else{
-                        etvv1.setError("Wrong Username");
-                        etvv1.requestFocus();
-                    }
-
-                }
-                else{
-                    etvv1.setError("Wrong Username");
-                    etvv1.requestFocus();
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
+            public void onClick(View v) {
+                Intent intent = new Intent(emergency1.this,Home.class);
+                startActivity(intent);
             }
         });
 
+        btnnn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Reference = FirebaseDatabase.getInstance().getReference().child("User");
+
+            }
+        });
+        btnnn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(emergency1.this,emergency2.class);
+                startActivity(intent);
+            }
+        });
 
     }
 
-    private void showAllUserData(){
-        Intent intent = getIntent();
-        int[] user_Telephone = intent.getIntArrayExtra("tel");
-        String user_address = intent.getStringExtra("address");
-    }
 
 }
--- app/src/main/java/com/example/mad/emergency2.java
+++ app/src/main/java/com/example/mad/emergency2.java
@@ -2,13 +2,17 @@ package com.example.mad;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class emergency2 extends AppCompatActivity {
 
     Button btnnn6, btnnn7, btnnn8;
+    ImageButton imagebtn20;
     TextView tvv3, tvv4;
 
     @Override
@@ -19,7 +23,16 @@ public class emergency2 extends AppCompatActivity {
         btnnn6 = findViewById(R.id.btnn6);
         btnnn7 = findViewById(R.id.btnn7);
         btnnn8 = findViewById(R.id.btnn8);
+        imagebtn20 = findViewById(R.id.imageButton20);
         tvv3 = findViewById(R.id.tv3);
         tvv4 = findViewById(R.id.tv4);
+
+        imagebtn20.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(emergency2.this,emergency1.class);
+                startActivity(intent);
+            }
+        });
     }
 }
