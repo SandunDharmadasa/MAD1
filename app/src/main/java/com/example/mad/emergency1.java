dd40af639b9ef1a9b94d945e3d7fb14ba8bd25fa
JANINDU MALEESHA <janindumaleesha99@gmail.com>
2021-05-07 13:40:32 +0530

    put the ID buttons and textview and Edittextview

--- app/src/main/java/com/example/mad/emergency1.java
+++ app/src/main/java/com/example/mad/emergency1.java
@@ -3,12 +3,29 @@ package com.example.mad;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class emergency1 extends AppCompatActivity {
+
+    Button btnnn1, btnnn2, btnnn3, btnnn4, btnnn5;
+    EditText etvv1;
+    TextView tvv1, tvv2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_emergency1);
+
+        btnnn1 = findViewById(R.id.btnn1);
+        btnnn2 = findViewById(R.id.btnn2);
+        btnnn3 = findViewById(R.id.btnn3);
+        btnnn4 = findViewById(R.id.btnn4);
+        btnnn5 = findViewById(R.id.btnn5);
+        etvv1 = findViewById(R.id.etv1);
+        tvv1 = findViewById(R.id.tv1);
+        tvv2 = findViewById(R.id.tv2);
+
     }
 }
--- app/src/main/java/com/example/mad/emergency2.java
+++ app/src/main/java/com/example/mad/emergency2.java
@@ -3,12 +3,23 @@ package com.example.mad;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class emergency2 extends AppCompatActivity {
+
+    Button btnnn6, btnnn7, btnnn8;
+    TextView tvv3, tvv4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_emergency2);
+
+        btnnn6 = findViewById(R.id.btnn6);
+        btnnn7 = findViewById(R.id.btnn7);
+        btnnn8 = findViewById(R.id.btnn8);
+        tvv3 = findViewById(R.id.tv3);
+        tvv4 = findViewById(R.id.tv4);
     }
 }
