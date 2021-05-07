13e162622c3782322334e6b8ed5d422151587469
JANINDU MALEESHA <janindumaleesha99@gmail.com>
2021-05-07 19:35:12 +0530

    created calculation part and enter the new button

--- app/src/main/java/com/example/mad/emergency2.java
+++ app/src/main/java/com/example/mad/emergency2.java
@@ -11,9 +11,10 @@ import android.widget.TextView;
 
 public class emergency2 extends AppCompatActivity {
 
-    Button btnnn6, btnnn7, btnnn8;
+    Button btnnn6, btnnn7, btnnn8, btnnn9;
     ImageButton imagebtn20;
     TextView tvv3, tvv4;
+    int counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +24,7 @@ public class emergency2 extends AppCompatActivity {
         btnnn6 = findViewById(R.id.btnn6);
         btnnn7 = findViewById(R.id.btnn7);
         btnnn8 = findViewById(R.id.btnn8);
+        btnnn9 = findViewById(R.id.btnn9);
         imagebtn20 = findViewById(R.id.imageButton20);
         tvv3 = findViewById(R.id.tv3);
         tvv4 = findViewById(R.id.tv4);
@@ -34,5 +36,34 @@ public class emergency2 extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        btnnn6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counter++;
+                tvv3.setText(Integer.toString(counter));
+            }
+        });
+
+        btnnn7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(counter <= 0){
+                    counter = 0;
+                }else{
+                    counter--;
+                    tvv3.setText(Integer.toString(counter));
+                }
+            }
+        });
+        btnnn9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int mu = Integer.parseInt(tvv3.getText().toString()) * 2500;
+                tvv4.setText("results is "+mu);
+            }
+        });
+
     }
+
 }
