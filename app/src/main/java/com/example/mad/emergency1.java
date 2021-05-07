289b6180dbddd4a2bc77b199f2c7ba547c9a7240
JANINDU MALEESHA <janindumaleesha99@gmail.com>
2021-05-07 22:51:25 +0530

    create validation part

--- app/src/main/java/com/example/mad/emergency1.java
+++ app/src/main/java/com/example/mad/emergency1.java
@@ -1,5 +1,6 @@
 package com.example.mad;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -10,18 +11,22 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
-
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
-public class emergency1 extends AppCompatActivity {
+
+public class emergency1<AuthResult> extends AppCompatActivity {
 
     Button btnnn1, btnnn2, btnnn3, btnnn4, btnnn5;
     ImageButton imagebtn2;
     EditText etvv1;
     TextView tvv1, tvv2;
     DatabaseReference Reference;
+    FirebaseDatabase mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,7 +55,21 @@ public class emergency1 extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                Reference = FirebaseDatabase.getInstance().getReference().child("User");
+                Reference = FirebaseDatabase.getInstance().getReference().child("User").child("1");
+                Reference.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        String tel = snapshot.child("tel").getValue().toString();
+                        String address = snapshot.child("address").getValue().toString();
+                        tvv1.setText(tel);
+                        tvv2.setText(address);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
 
             }
         });
@@ -63,6 +82,7 @@ public class emergency1 extends AppCompatActivity {
         });
 
     }
+
 
 
 }
